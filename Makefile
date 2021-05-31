441de31de8454b2ee529e0b570cfd9e49b797e44
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-05-31 06:06:45 +0200

    basics: data types

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@
 all:
 	python3.9 conditions.py ## if - else
 	python3.9 py_hello.py
+	python3.9 data_types.py
---
+++ data_types.py
@@ -0,0 +1,25 @@
+#
+# data types
+#
+# type()
+
+name = "Sohaib"
+age = 55
+tall = 1.5
+long_num = 1000000000000000000000000000000000000000000000000000000
+long_num += 1
+sma_list = [1,2,3,4,5]
+sma_tuple = (1,2,3,4,5)
+sma_dect = { "one": 1, "two": 2, "three": 3}
+
+print(type(name), name)
+print(type(age), age)
+print(type(tall), tall)
+print(type(long_num), long_num)
+print(type(sma_list), sma_list)
+print(type(sma_tuple), sma_tuple)
+print(type(sma_dect), sma_dect)
+print(type(2 == 2))
+print(type(2 == 4))
+
+
